4a57a3d3adc1686c10a4f534421630812de2b43c
saiprakashpatnala <99853021+saiprakashpatnala@users.noreply.github.com>
2022-11-16 13:31:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import{Switch,Route} from 'react-router-dom"
-
+import BrandDetails from './components/BrandsSection
 const App = () => (
 <div>
+  <Switch>
+  <Route path="/" component={BrandsSection}/>
+  </Switch>
   
 </div>
 )
